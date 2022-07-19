54eaeef19418beec1a3f26db55775623b7ae84e9
MisterKapuster <gdimitar7@gmail.com>
2022-07-19 09:11:45 +0300

    lesson_37

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,27 @@ import { Post } from './Post/Post';
 
 export const MyPosts = (props) => {
 	let postsElements = props.posts.map((item, i) => <Post key={i} message={item.message} likes={item.likes} />);
+
+	let newPostElement = React.createRef();
+
+	let addPost = () => { 
+		props.addPost();
+	}
+
+	let onPostChange = () => { 
+		let text = newPostElement.current.value;
+		props.updateNewPostText(text);
+	}
+
 	return (
 		<div className={s.postsBlock}>
 			<h3>My posts</h3>
 			<div>
 				<div>
-					<textarea></textarea>
+					<textarea ref={newPostElement} onChange={onPostChange} value={props.newPostText} />
 				</div>
 				<div>
-					<button>Add post</button>
+					<button onClick={addPost}>Add post</button>
 				</div>
 			</div>
 			<div className={s.posts}>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,24 +1,50 @@
-let state = {
-	profilePage: {
-		posts: [
-			{ message: "Hi, how are you?", likes: "10" },
-			{ message: "It's my first post!", likes: "25" }
-		]
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ message: "Hi, how are you?", likes: "10" },
+				{ message: "It's my first post!", likes: "25" }
+			],
+			newPostText: "it-kabzda.com"
+		},
+		dialogsPage: {
+			dialogsData: [
+				{ id: 1, name: 'Dimych' },
+				{ id: 2, name: 'Andrey' },
+				{ id: 3, name: 'Sveta' },
+				{ id: 4, name: 'Valera' },
+			],
+			messagesData: [
+				{ id: 1, message: 'Hi' },
+				{ id: 2, message: 'How are You' },
+				{ id: 3, message: 'You' },
+				{ id: 4, message: 'Good Bye' },
+			]
+		}
 	},
-	dialogsPage: {
-		dialogsData: [
-			{ id: 1, name: 'Dimych' },
-			{ id: 2, name: 'Andrey' },
-			{ id: 3, name: 'Sveta' },
-			{ id: 4, name: 'Valera' },
-		],
-		messagesData: [
-			{ id: 1, message: 'Hi' },
-			{ id: 2, message: 'How are You' },
-			{ id: 3, message: 'You' },
-			{ id: 4, message: 'Good Bye' },
-		]
+	_callSubscriber() {
+		console.log('State changed');
+	},
+	getState() { 
+		return this._state;
+	},
+	addPost() {
+		let newPost = {
+			id: 3,
+			message: this._state.profilePage.newPostText,
+			likes: 0
+		};
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = '';
+		this._callSubscriber(this._state);
+	},
+	updateNewPostText(newText) {
+		this._state.profilePage.newPostText = newText;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
 	}
 }
 
-export default state;
+export default store;
