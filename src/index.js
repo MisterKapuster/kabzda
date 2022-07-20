e07f855d2566ec623f50856b0209726466a3319b
MisterKapuster <gdimitar7@gmail.com>
2022-07-20 16:34:00 +0300

    stateCopy

--- src/index.js
+++ src/index.js
@@ -7,18 +7,11 @@ import store from './redux/redux-store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let rerenderEntireTree = () => {
-	root.render(
-		<React.StrictMode>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</React.StrictMode>
-	);
-	reportWebVitals();
-}
-rerenderEntireTree();
-
-store.subscribe(() => { 
-	rerenderEntireTree();
-});
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
+reportWebVitals();
