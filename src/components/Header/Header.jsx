221efed85f47c504eba315044807df9d28f58b63
MisterKapuster <gdimitar7@gmail.com>
2022-07-27 17:46:30 +0300

    auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-export const Header = () => {
+export const Header = (props) => {
 	return (
 		<header className={s.header}>
 			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1667px-Apple_logo_black.svg.png" alt="logo" />
+
+			<div className={s.loginBlock}>
+				{ props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+			</div>
 		</header>
 	)
 }
